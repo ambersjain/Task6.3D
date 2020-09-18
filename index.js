64ba3bf7142a9942019a6a556ad6d41804c37a9f
Amber Jain <jainamb@deakin.edu.au>
2020-09-18 16:51:59 +1000

    Improved the worker routing directory structure

--- index.js
+++ index.js
@@ -12,14 +12,12 @@ const bcrypt = require('bcrypt');
 const passport = require('passport');
 const session = require('express-session');
 
-
+//getting the worker routes
+const workerRoutes = require('./routes/worker-routes')
 
 // User Model
 const User = require("./models/user");
 const { EEXIST } = require('constants');
-
-// Worker Model
-const Worker = require("./models/worker");
 
 //use Google auth in app
 /*
@@ -62,6 +60,9 @@ db.once('open', function () {
 
 // Need to use this to be able to use views folder
 app.use(express.static('views'));
+
+// Use the worker routes
+app.use(workerRoutes);
 
 // Home route
 app.get('/', (req, res) => {
@@ -191,98 +192,6 @@ app.post('/signin', (req, res) => {
     return res.status(200).send();
   })
 })
-
-
-/*
-    WORKER ROUTES
-*/
-
-
-/* USE GET, POST and DELETE TO
-  - Retrieve, add and remove workers respectively[URI: /workers]
-*/
-
-
-app.route('/workers')
-  .get((req, res) => {
-    Worker.find((err, workerList) => {
-      if (err) { res.send(err) }
-      else { res.send(workerList) }
-    })
-
-  })
-  .post((req, res) => {
-    const worker = new Worker({
-      id: req.body.id,
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-      address: req.body.address,
-      mobile_number: req.body.mobile_number,
-    });
-
-    worker.save((err) => {
-      if (err) { res.send(err); console.log("Unable to add the worker"); }
-      else { res.send("Worker Added Succesfully") }
-    })
-  })
-  .delete((req, res) => {
-    Worker.deleteMany((err) => {
-      if (err) { res.send(err) }
-      else { res.send("Successfully deleted all worker records!") }
-    })
-  });
-
-
-/* USE GET, PUT and DELETE TO
-  - Retrieve, update and remove a specific worker [URI: /workers/:id]
-*/
-
-/* USE PATCH TO
-  - Update specific worker’s address and mobile phone [URI: /workers/:id]
-  - Update a specific worker’s password [URI: /workers/:id]
-*/
-app.route('/workers/:id')
-  .get((req, res) => {
-    Worker.findOne({ id: req.params.id }, (err, foundWorker) => {
-      if (foundWorker) { res.send(foundWorker) }
-      else { res.send("Worker not found.") }
-    })
-  })
-  .put((req, res) => {
-    Worker.update(
-      { id: req.params.id },
-      {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        address: req.body.address,
-        mobile_number: req.body.mobile_number
-      },
-      { overwrite: true },
-      (err) => {
-        if (err) { res.send(err) }
-        else { res.send("Successfully Updated!") }
-      }
-    )
-  })
-  .delete((req, res) => {
-    Worker.deleteOne({ id: req.params.id }, (err, foundWorker) => {
-      if (foundWorker) { res.send(foundWorker) }
-      else { res.send("Worker account not deleted") }
-    })
-  })
-  .patch((req, res) => {
-    Worker.update(
-      { id: req.params.id },
-      { $set: req.body },
-      { overwrite: true },
-      (err) => {
-        if (err) { res.send(err) }
-        else { res.send("Successfully Updated!") }
-      }
-    )
-  })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
